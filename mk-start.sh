630a401185db494fdb81714ebec36fcfb735016f
Jean Britz <britz.jean@gmail.com>
2022-04-24 13:17:24 +0200

    Added hostname to access Kubenetes  API Server

--- mk-start.sh
+++ mk-start.sh
@@ -3,7 +3,7 @@
 set -e
  
 echo "Starting Minikube"
-minikube start --driver=virtualbox --kubernetes-version=v1.20.0 --memory=8192 --cpus=4
+minikube start --driver=virtualbox --apiserver-names=minikube --kubernetes-version=v1.20.0 --memory=8192 --cpus=4
  
 minikube_ip=$(minikube ip)
 echo "Your Minikube IP: $minikube_ip"
