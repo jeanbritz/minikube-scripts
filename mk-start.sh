b7c8402ac85f9fb4979b23f61976fb077d16934a
Jean Britz <britz.jean@gmail.com>
2022-04-25 22:23:52 +0200

    Added ingress addon to startup script

--- mk-start.sh
+++ mk-start.sh
@@ -3,7 +3,13 @@
 set -e
  
 echo "Starting Minikube"
-minikube start --driver=virtualbox --apiserver-names=minikube --kubernetes-version=v1.20.0 --memory=8192 --cpus=4
+minikube start \
+    --driver=virtualbox \
+    --apiserver-names=minikube \
+    --kubernetes-version=v1.23.3 \
+    --addons=ingress \
+    --memory=8192 \
+    --cpus=4
  
 minikube_ip=$(minikube ip)
 echo "Your Minikube IP: $minikube_ip"
